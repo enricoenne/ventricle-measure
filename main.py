bcf17c1629380b2c7170e0c979a5a84e1ce88a4b
enricoenne <enrico.negri97@gmail.com>
2025-09-11 16:11:33 +0200

    adding lines to connect external points to ventricular reference

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from scipy.ndimage import distance_transform_edt, center_of_mass, label
 from aicsimageio import AICSImage
 import matplotlib.pyplot as plt
 
+from skimage.draw import line
+
 import math
 
 def get_picture(path):
@@ -42,6 +44,45 @@ def show_pic(pic, title = ''):
     plt.title(title)
     plt.show()
 
+def get_valid(arr, height, width):
+    valid = (arr[:,1] >= 0) & (arr[:,1] < height) & \
+        (arr[:,0] >= 0) & (arr[:,0] < width)
+    
+    return arr[valid]
+
+def get_ray_pixels(matrix, center, angle, res):
+
+    max_length = np.linalg.norm(matrix.shape)
+
+    r = np.arange(0, max_length)
+    # all points along the ray
+    x = ventr_coords[0] + r * np.cos(a)
+    y = ventr_coords[1] + r * np.sin(a)
+    pixels = np.vstack((y.astype(int), x.astype(int))).T
+
+    pixels = get_valid(pixels, *matrix.shape)
+
+    return(pixels)
+
+
+
+def get_circle_pixels(matrix, center, radius, res):
+    rows, cols = matrix.shape
+    y, x = np.ogrid[:rows, :cols]
+    cx, cy = center
+    
+    
+    dist = np.sqrt(((y - cy)*res[1])**2 + ((x - cx)*res[0])**2)
+
+    mask = np.isclose(dist, radius, atol=1)  # tolerance because exact match is rare
+
+    return np.argwhere(mask)
+
+def get_line_pixels(p1, p2):
+    x0, y0 = p1
+    x1, y1 = p2
+    rr, cc = line(y0, x0, y1, x1)  # note: line() expects (row, col)
+    return rr, cc
 
 donut, res = get_picture2D('donut.tif')
 point, _ = get_picture('point.tif')
@@ -70,59 +111,76 @@ distance = np.zeros_like(donut, dtype=np.float64)
 # keeping it only in the area of the donut
 distance[donut_mask] = distance_transform_edt(~ventr_mask, sampling= res)[donut_mask]
 
-# show_pic(distance)
+# max value in the distance pic
+max_dist = np.max(distance)
 
+
+# find center of the ventricle
 ventr_coords = center_of_mass(ventr_mask)
 
 
-
+# find point in the reference pic
 point_mask = point != 0
 point_coords = center_of_mass(point_mask)
 
-
-# plt.imshow(distance)
-# plt.scatter(ventr_coords[1], ventr_coords[0], color='#FF8800')
-# plt.scatter(point_coords[1], point_coords[0], color='#008800')
-# plt.show()
 
 dx = point_coords[0] - ventr_coords[0]
 dy = point_coords[1] - ventr_coords[1]
 angle_0 = math.atan2(dy, dx)
 
 max_length = np.linalg.norm(donut.shape)
+# how many angles
 n_angles = 40
 
-results = pd.DataFrame(columns = ['angle_r', 'angle_d', 'thickness'])
+results = pd.DataFrame(columns = ['angle_r', 'angle_d', 'thickness', 'ext_x', 'ext_y', 'vent_x', 'vent_y'])
 
 test_img = distance.copy()
 
 for i in range(n_angles):
+
+    # angle of the ray
     a = 2*math.pi / n_angles * i + angle_0
+    
+    line_pixels = get_ray_pixels(donut, point_coords, a, res)
 
-    r = np.arange(0, max_length)
-
-    x = ventr_coords[0] + r * np.cos(a)
-    y = ventr_coords[1] + r * np.sin(a)
-
-    pixels = np.vstack((y.astype(int), x.astype(int))).T
-
-    valid = (pixels[:,1] >= 0) & (pixels[:,1] < height) & \
-            (pixels[:,0] >= 0) & (pixels[:,0] < width)
-    pixels = pixels[valid]
+    cols = line_pixels[:,0]
+    rows = line_pixels[:,1]
 
 
-    cols = pixels[:,0]
-    rows = pixels[:,1]
 
-    test_img[rows, cols] = 2000
+    # add line on top of picture
+    # test_img[rows, cols] = max_dist
 
+    # find the max ventricle distance along line
     current_thickness = np.max(distance[rows, cols])
+    # where is the max distance along line
+    current_index = np.argmax(distance[rows, cols])
+    current_coords = [int(cols[current_index]), int(rows[current_index])]
 
-    results.loc[len(results)] = [a-angle_0, np.degrees(a-angle_0), current_thickness]
+    # circe centered in current point
+    circ_pixels = get_circle_pixels(distance, current_coords, current_thickness, res)
+    rows = circ_pixels[:,0]
+    cols = circ_pixels[:,1]
+
+    # add circle on top of picture
+    # test_img[rows, cols] = max_dist
+
+    # I need to set the pixels outside the tissue at some max value
+    fake_distance = distance.copy()
+    fake_distance[outside_mask] = max_dist
+    vent_index = np.argmin(fake_distance[rows, cols])
+    vent_coords = [int(cols[vent_index]), int(rows[vent_index])]
+
+    r_line, c_line = get_line_pixels(current_coords, vent_coords)
+    test_img[r_line, c_line] = max_dist
+
+    results.loc[len(results)] = [a-angle_0, np.degrees(a-angle_0), current_thickness, *current_coords, *vent_coords]
 
 # print(results)
 
-plt.imshow(test_img)
+plt.imshow(test_img, cmap = 'magma')
+plt.scatter(results['ext_x'], results['ext_y'], s = 10, color = 'cyan')
+plt.scatter(results['vent_x'], results['vent_y'], s = 10, color = 'cyan')
 plt.show()
 
 results.to_csv('result.csv')
